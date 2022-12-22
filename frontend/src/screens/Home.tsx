c61f56b3c688007b4d1fa99640e09a3e5f98e455
gregorsulcer <gregor.sulcer@student.um.si>
2022-12-22 20:11:53 +0100

    development branch

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,10 +1,28 @@
-import {View, Text, Button} from "react-native";
+import {View, Text, Button, Alert} from "react-native";
 import {FC} from "react";
 import {SafeAreaView} from "react-native-safe-area-context";
 interface HomeProps{
     navigation: any;
 }
 
+const showAlert = () =>
+    Alert.alert(
+        "Test",
+        "This is a test alert",
+        [
+            {
+                text: "Cancel",
+                onPress: () => Alert.alert("Cancel Pressed"),
+            },
+        ],
+        {
+            cancelable: true,
+            onDismiss: () =>
+                Alert.alert(
+                    "This alert was dismissed by tapping outside of the alert dialog."
+                ),
+        }
+    );
 const Home:FC<HomeProps> = ({ navigation }) => {
     return (
         <SafeAreaView className='flex-1'>
@@ -12,6 +30,7 @@ const Home:FC<HomeProps> = ({ navigation }) => {
                 <Text className='text-2xl m-3'>Home</Text>
                 <Button
                     title="Click Here"
+                    onPress={showAlert}
                 />
             </View>
         </SafeAreaView>
