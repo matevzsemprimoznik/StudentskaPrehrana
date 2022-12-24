375f6b32c37d840ea2087bffc85eab57633fd71c
Matevz <semprimoznik.matevz@gmail.com>
2022-12-24 15:54:57 +0100

    fix custom layout shift when keyboard is shown

--- frontend/src/components/CustomLayoutHeader.tsx
+++ frontend/src/components/CustomLayoutHeader.tsx
@@ -7,7 +7,7 @@ export interface CustomLayoutHeaderProps {
 }
 
 const CustomLayoutHeader:FC<CustomLayoutHeaderProps> = ({children}) => {
-    return <View className='h-2/6 bg-custom-yellow'>
+    return <View className='h-60 bg-custom-yellow'>
         <Image className='w-5/6 m-auto mt-5 absolute -z-1' source={BackgroundImage}/>
         {children}
     </View>
