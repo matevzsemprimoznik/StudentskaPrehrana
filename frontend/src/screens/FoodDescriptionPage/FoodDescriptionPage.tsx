2f4416c4889c5445733f00fcded001ee43af4aa9
Matevz <semprimoznik.matevz@gmail.com>
2022-12-26 15:15:08 +0100

    fix food description image position

--- frontend/src/screens/FoodDescriptionPage/FoodDescriptionPage.tsx
+++ frontend/src/screens/FoodDescriptionPage/FoodDescriptionPage.tsx
@@ -1,5 +1,5 @@
 import {FC} from 'react';
-import {Text, View} from "react-native";
+import {ScrollView, Text, View} from "react-native";
 import {translate} from "../../utils/translations/translate";
 import CustomLayout from "../../components/CustomLayout";
 import FoodImageCircle from "./FoodImageCircle";
@@ -27,30 +27,35 @@ const FoodDescriptionPage:FC<FoodDescriptionProps> = () => {
                     <ArrowLongLeftIcon color='white'/>
                 </View>
                 <View className='flex items-center mt-8 pt-8 relative'>
-                    <FoodImageCircle foodImage={menu.image}/>
+
                 </View>
             </CustomLayout.Header>
             <CustomLayout.Main>
-                <View className='mx-2 flex-1'>
-                    <Text className='text-lg font-medium text-center mb-5 mt-6 ml-2.5'>{menu.name}</Text>
-                    <View className='items-center'>
-                        <View className='flex-row gap-1'>
-                            <Rating rating={4.8} numberOfReviews={230} color={'text-custom-black'}/>
-                            <Price classname={'ml-7'} price={3.60}/>
+                <View className='flex-1 -mt-24'>
+                    <FoodImageCircle foodImage={menu.image}/>
+                    <View className='mx-2 flex-1 mt-6'>
+                        <Text className='text-lg font-medium text-center mb-5 mx-2.5'>{menu.name}</Text>
+                        <View className='items-center'>
+                            <View className='flex-row gap-1'>
+                                <Rating rating={4.8} numberOfReviews={230} color={'text-custom-black'}/>
+                                <Price classname={'ml-7'} price={3.60}/>
+                            </View>
                         </View>
+                        <ScrollView className='mt-6'>
+                            <Text className='text-base font-medium mb-5 ml-2.5'>{translate('food-description-header')}</Text>
+                            <View className='mx-2'>
+                                <Text className='opacity-50'>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation  "}</Text>
+                            </View>
+                            <Text className='text-base font-medium mb-5 mt-6 ml-2.5'>{translate('food-allergens-header')}</Text>
+                            <View className='mx-2 mx-4'>
+                                {menu.allergens.map((allergen, index) =>
+                                    <ListItem key={index} text={allergen}/>
+                                )}
+                            </View>
+                            <Text className='text-lg font-medium mb-5 mt-6 ml-2.5'>{translate('food-picture-header')}</Text>
+                            <ImageList images={menu.images}/>
+                        </ScrollView>
                     </View>
-                    <Text className='text-lg font-medium mb-5 mt-6 ml-2.5'>{translate('food-description-header')}</Text>
-                    <View className='mx-2'>
-                        <Text className='opacity-50'>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation  "}</Text>
-                    </View>
-                    <Text className='text-lg font-medium mb-5 mt-6 ml-2.5'>{translate('food-allergens-header')}</Text>
-                    <View className='mx-2 mx-4'>
-                        {menu.allergens.map((allergen, index) =>
-                            <ListItem key={index} text={allergen}/>
-                        )}
-                    </View>
-                    <Text className='text-lg font-medium mb-5 mt-6 ml-2.5'>{translate('food-picture-header')}</Text>
-                    <ImageList images={menu.images}/>
                 </View>
             </CustomLayout.Main>
         </CustomLayout>
