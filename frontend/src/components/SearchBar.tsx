a52e6216a93bbfca78e651721a19e4a5cef0b5ac
gregorsulcer <gregor.sulcer@student.um.si>
2022-12-27 13:58:23 +0100

    fix screens

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -4,11 +4,11 @@ import { MagnifyingGlassIcon } from "react-native-heroicons/solid";
 
 const SearchBar = () => {
     return (
-        <View className='w-full'>
-            <View className='absolute top-1 scale-110 left-2 z-20'>
+        <View className='w-full top-3'>
+            <View className='absolute top-1.5 scale-110 left-2 z-20'>
                 <MagnifyingGlassIcon fill="black" size={16}/>
             </View>
-            <TextInput className='bg-custom-white rounded-full w-full p-[5px] pl-10' placeholder={translate('home-header-search')}/>
+            <TextInput className='bg-custom-white rounded-full w-full p-[5px] pl-8' placeholder={translate('home-header-search')}/>
         </View>
     )
 }
--- frontend/src/utils/translations/translations.ts
+++ frontend/src/utils/translations/translations.ts
@@ -1,11 +1,14 @@
 export const translations = {
     sl: {
-        'home-header': 'Kaj želiš malicati danes?',
+        'home-header': 'Kje želiš malicati danes?',
         'home-header-search': 'Poišči restavracijo',
         'home-main-title': 'Restavracije',
         'food-description-header': 'Opis jedi',
         'food-allergens-header': 'Alergeni',
-        'food-picture-header': 'Slike',
+        'food-picture-header': 'Slike jedi',
         'restaurant-main-title': 'Jedi',
-        'restaurant-main-comments': 'Komentarji',}
+        'restaurant-main-comments': 'Komentarji',
+        'no-allergens': 'Alergeni niso podani',
+        'comments-header': 'Komentarji',
+    }
 }
