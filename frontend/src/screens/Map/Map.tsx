4026833c1de1d918cf8b425177058049a5286c79
Matevz <semprimoznik.matevz@gmail.com>
2023-01-08 11:46:23 +0100

    fixed map display in development environment

--- frontend/src/screens/Map/Map.tsx
+++ frontend/src/screens/Map/Map.tsx
@@ -1,72 +1,82 @@
-import MapView, {Callout, Marker} from "react-native-maps";
-import style from './mapStyle.json'
 import {FC, useEffect, useMemo, useRef, useState} from "react";
 import * as Location from "expo-location";
-import {Text, Image, View} from "react-native";
-import {translate} from "../../utils/translations/translate";
-import LocationMark from "../../assets/location-mark.png";
-import MapboxGL from '@rnmapbox/maps';
 import {CameraRef} from "@rnmapbox/maps/lib/typescript/components/Camera";
-
-MapboxGL.setAccessToken('pk.eyJ1IjoibWF0ZXZ6IiwiYSI6ImNsY2dmb3l4czA5YjkzbmxjMTAyank0aHMifQ.CyFCzwJ9_VfYh-CQ1Od79g');
+import {View, Text} from "react-native";
 
 interface MapProps {
     route: any;
 }
 
+const getMapbox = async () => {
+    if (!__DEV__) {
+        const mapbox = await import('@rnmapbox/maps')
+        const MapboxGL = mapbox.default
+        await MapboxGL.setAccessToken('pk.eyJ1IjoibWF0ZXZ6IiwiYSI6ImNsY2dmb3l4czA5YjkzbmxjMTAyank0aHMifQ.CyFCzwJ9_VfYh-CQ1Od79g');
+        const {MapView, PointAnnotation, Camera, UserLocation} = MapboxGL
+        return {MapView, PointAnnotation, Camera, UserLocation}
+    }
+    return null
+}
+
 const Map: FC<MapProps> = ({route}) => {
     const camera = useRef<CameraRef>(null);
     const [currentUserLocation, setCurrentUserLocation] = useState<{ longitude: number, latitude: number } | null>();
+    const GenericMapComponent = useMemo(() => <View className='w-full h-full flex-row justify-center'
+                                                    style={{alignItems: 'center'}}><Text>Mapa</Text></View>, [])
+    const [NativeMapComponent, setNativeMapComponent] = useState<JSX.Element | null>(null)
 
     const getUserLocation = async () => {
-        const {status} = await Location.requestForegroundPermissionsAsync();
+        try{
+            const {status} = await Location.requestForegroundPermissionsAsync();
 
-        if (status !== 'granted') {
-            return;
+            if (status !== 'granted') {
+                return;
+            }
+
+            const location = await Location.getCurrentPositionAsync();
+            setCurrentUserLocation({
+                longitude: location.coords.longitude,
+                latitude: location.coords.latitude
+            })
         }
-        const location = await Location.getCurrentPositionAsync();
-        setCurrentUserLocation({
-            longitude: location.coords.longitude,
-            latitude: location.coords.latitude
-        })
+        catch (e) {
+            console.log(e)
+        }
+
+    }
+
+    const setMapComponentAsync = async () => {
+        let MapboxGL = await getMapbox();
+
+        setNativeMapComponent(() => MapboxGL ? <MapboxGL.MapView style={{flex: 1}}>
+            <MapboxGL.Camera
+                ref={camera}
+                zoomLevel={12}
+                centerCoordinate={[route.params.longitude, route.params.latitude]}
+                animationMode='none'
+            />
+            <MapboxGL.UserLocation visible/>
+            <MapboxGL.PointAnnotation id='restaurant_location' key='restaurant_location'
+                                      coordinate={[route.params.longitude, route.params.latitude]}/>
+        </MapboxGL.MapView> : null)
 
     }
 
     useEffect(() => {
         getUserLocation()
+        setMapComponentAsync()
     }, [])
 
     useEffect(() => {
         if (camera.current && currentUserLocation) {
             camera.current.fitBounds([currentUserLocation.longitude, route.params.latitude], [route.params.longitude, currentUserLocation.latitude], 50, 1000)
         }
-    }, [currentUserLocation])
+    }, [currentUserLocation, NativeMapComponent])
 
-    return <MapboxGL.MapView style={{flex: 1}}>
-        <MapboxGL.Camera
-            ref={camera}
-            zoomLevel={12}
-            followUserLocation
-            centerCoordinate={[route.params.longitude, route.params.latitude]}
-        />
-        <MapboxGL.PointAnnotation id='restaurant_location' key='restaurant_location' coordinate={[route.params.longitude, route.params.latitude]} />
-        {currentUserLocation && <MapboxGL.PointAnnotation
-            key="user_location"
-            id="user_location"
-            coordinate={[currentUserLocation.longitude, currentUserLocation.latitude]}
-        >
-            <View
-                style={{
-                    height: 20,
-                    width: 20,
-                    backgroundColor: "#1d8ef5",
-                    borderRadius: 50,
-                    borderColor: "#ffffff",
-                    borderWidth: 3,
-                }}
-            />
-        </MapboxGL.PointAnnotation>}
-    </MapboxGL.MapView>
+    if(NativeMapComponent)
+        return NativeMapComponent
+
+    return GenericMapComponent
 }
 
 export default Map
